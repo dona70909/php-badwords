a53af4c5ea5022e8dfde0ba2456a03f2a282a25f
Donatella7 <campaniellodonatella@gmail.com>
2022-04-19 15:22:36 +0200

    dot

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php 
-    $text = "PHP is a general-purpose scripting language geared toward web development. It was originally created by Danish-Canadian programmer Rasmus Lerdorf in 1994. The PHP reference implementation is now produced by The PHP Group. PHP originally stood for Personal Home Page, but it now stands for the recursive initialism PHP: Hypertext Preprocessor";
+    $text = "PHP is a general-purpose scripting language geared toward web development. It was originally created by Danish-Canadian programmer Rasmus Lerdorf in 1994. The PHP reference implementation is now produced by The PHP Group. PHP originally stood for Personal Home Page, but it now stands for the recursive initialism PHP: Hypertext Preprocessor. ";
     $bad_word = $_GET["word"];
     $stars = "***";
 
